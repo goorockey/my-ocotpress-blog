5ce3e9631bbd9dbfcd922b195fba18a4b551a8ba
154061424@qq.com <154061424@qq.com>
2012-10-19 22:12:16 +0800

    footer

---
+++ plugins/footer_url.rb
@@ -0,0 +1,30 @@
+# post_footer_filter.rb
+# Append every post some footer infomation like original url 
+# Kevin Lynx
+# 7.26.2012
+#
+require './plugins/post_filters'
+
+module AppendFooterFilter
+    def append(post)
+        author = post.site.config['author']
+        url = post.site.config['url']
+        pre = post.site.config['original_url_pre']
+        post.content + %Q[<br/>
+            <br/>
+            #{pre or "original link:"}
+            <a href='#{post.full_url}'>#{post.full_url}</a>
+            ]
+    end 
+end
+
+module Jekyll
+    class AppendFooter < PostFilter
+        include AppendFooterFilter
+        def pre_render(post)
+            post.content = append(post) if post.is_post?
+        end
+    end
+end
+
+Liquid::Template.register_filter AppendFooterFilter
